f5f0499945acffc7f3fbaffd9d08b260e764f898
Peterkanume <137292405+Peterkanume@users.noreply.github.com>
2024-11-06 15:12:54 +0300

    Update App.js
    Addition of the hero in the app.js and capitalizing the neccessary.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,28 +2,29 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShopCategory from './pages/ShopCategory';
-import Hero from './components/Hero/Hero';
+import Hero from './components/Hero/Hero';  // Import Hero
 import Product from './pages/Product';
 import LoginSignup from './pages/LoginSignup';
-import cart from './pages/cart'; 
-
-
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
-      
-        <Routes> {/* Capitalize Routes */}
+        <Navbar /> {/* This will render the Navbar component */}
+        
+        {/* Render Hero component here */}
+        <Hero />  {/* This is where the Hero section is rendered */}
+
+        <Routes>
           <Route path="/" element={<ShopCategory category="Shop" />} />
           <Route path="/samsung" element={<ShopCategory category="samsung" />} />
           <Route path="/iphone" element={<ShopCategory category="iphone" />} />
           <Route path="/pixel" element={<ShopCategory category="pixel" />} />
           <Route path="/product">
-            <Route path=":productId" element={<Product />} /> {/* Proper nesting for dynamic routes */}
+            <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path="/cart" element={<Product />} /> {/* Ensure Cart component is capitalized if it's defined */}
+          <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
